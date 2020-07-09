092aa1f53978918958e20441cd62f2add71475e7
Doris Hsiao <dorishsiao007@gmail.com>
2020-07-09 16:57:15 +0800

    create ui template

---
+++ all.js
@@ -0,0 +1,31 @@
+new Vue(
+    {
+        el: "#product",
+        data: {
+            productData: [
+                {
+                    title: "Switch",
+                    category: "掌上主機",
+                    content: "Switch遊戲主機",
+                    description: "Switch遊戲主機",
+                    imageUrl: "",
+                    enabled: true,
+                    origin_price: 9870,
+                    price: 9800,
+                    unit: "台"
+                },
+                {
+                    title: "PlayStation 4<",
+                    category: "主機",
+                    content: "PlayStation 4<主機",
+                    description: "PlayStation 4<主機",
+                    imageUrl: "",
+                    enabled: true,
+                    origin_price: 10980,
+                    price: 10900,
+                    unit: "台"
+                },
+            ]
+        }
+    }
+);
