0462e92bec1a0c11e8bc3522b6f57657c7eb79c2
Doris Hsiao <dorishsiao007@gmail.com>
2020-07-09 23:51:33 +0800

    update

--- all.js
+++ all.js
@@ -1,31 +1,40 @@
-new Vue(
-    {
-        el: "#product",
-        data: {
-            productData: [
-                {
-                    title: "Switch",
-                    category: "掌上主機",
-                    content: "Switch遊戲主機",
-                    description: "Switch遊戲主機",
-                    imageUrl: "",
-                    enabled: true,
-                    origin_price: 9870,
-                    price: 9800,
-                    unit: "台"
-                },
-                {
-                    title: "PlayStation 4<",
-                    category: "主機",
-                    content: "PlayStation 4<主機",
-                    description: "PlayStation 4<主機",
-                    imageUrl: "",
-                    enabled: true,
-                    origin_price: 10980,
-                    price: 10900,
-                    unit: "台"
-                },
-            ]
+new Vue({
+    el: "#product",
+    data: {
+        productData: [{
+                title: "Switch",
+                category: "掌上主機",
+                content: "Switch遊戲主機",
+                description: "Switch遊戲主機",
+                imageUrl: "",
+                enabled: true,
+                origin_price: 9870,
+                price: 9800,
+                unit: "台",
+                imgUrl: ""
+            },
+            {
+                title: "PlayStation 4",
+                category: "主機",
+                content: "PlayStation 4主機",
+                description: "PlayStation 4主機",
+                imageUrl: "",
+                enabled: true,
+                origin_price: 10980,
+                price: 10900,
+                unit: "台",
+                imgUrl: "https://www.us3c.com.tw/wp-content/uploads/2017/01/PS4-PRO-1-US3C.jpg"
+            },
+        ],
+        tempProduct: {}
+    },
+    methods: {
+        addProduct() {
+            const id = new Date().getTime();
+            this.tempProduct.id = id;
+            console.log(id);
+            console.log(this.tempProduct);
+            this.productData.push(this.tempProduct);
         }
-    }
-);
+    },
+});
