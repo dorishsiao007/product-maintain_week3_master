117fcb66f62036f38c9f2100869e4ca700622e8d
Doris Hsiao <dorishsiao007@gmail.com>
2020-07-16 11:42:09 +0800

    add/update/delete function

--- all.js
+++ all.js
@@ -1,7 +1,9 @@
 new Vue({
     el: "#product",
     data: {
-        productData: [{
+        productData: [
+            {
+                id: 1,
                 title: "Switch",
                 category: "掌上主機",
                 content: "Switch遊戲主機",
@@ -11,9 +13,10 @@ new Vue({
                 origin_price: 9870,
                 price: 9800,
                 unit: "台",
-                imgUrl: ""
+                imgUrl: "https://image.shutterstock.com/image-vector/video-game-console-gamepad-vector-260nw-1137618860.jpg"
             },
             {
+                id: 2,
                 title: "PlayStation 4",
                 category: "主機",
                 content: "PlayStation 4主機",
@@ -29,12 +32,54 @@ new Vue({
         tempProduct: {}
     },
     methods: {
-        addProduct() {
-            const id = new Date().getTime();
-            this.tempProduct.id = id;
-            console.log(id);
-            console.log(this.tempProduct);
-            this.productData.push(this.tempProduct);
+        updateProduct: function () {
+            if(this.tempProduct.id){
+                // Update Product Data
+                const id = this.tempProduct.id;
+                this.productData.forEach((item, index) => {
+                    if (item.id === id) {
+                        this.productData[index] = this.tempProduct;
+                    }
+                });
+                this.tempProduct = {};
+            }
+            else{
+                // Add Product Data
+                const id = new Date().getTime();
+                this.tempProduct.id = id;
+                this.productData.push(this.tempProduct);
+                this.tempProduct = {};
+            }
+        },
+
+        deleteProduct: function () {
+            const id = this.tempProduct.id;
+            this.productData.forEach((item, index) => {
+                if (item.id === id) {
+                    this.productData.splice(index, 1);
+                }
+            });
+            this.tempProduct = {};
+        },
+
+        openModal: function (action, data) {
+            switch (action) {
+                case "add": {
+                    this.tempProduct = {};
+                    break;
+                }
+                case "update": {
+                    this.tempProduct = JSON.parse(JSON.stringify(data));
+                    break;
+                }
+                case "delete": {
+                    this.tempProduct = JSON.parse(JSON.stringify(data));
+                    break;
+                }
+                default: {
+                    break;
+                }
+            }
         }
     },
 });
